be9ee908c3dd3eca9a8ab806d7f6d0112a859a3c
Pavel Lubnin <lubnin@42dev.ru>
2020-12-03 13:23:40 +0500

    выполнено задание по пагинации

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,3 +1,7 @@
+import csv
+
+from django.conf import settings
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -7,13 +11,32 @@ def index(request):
 
 
 def bus_stations(request):
-    current_page = 1
-    next_page_url = 'write your url'
-    return render(request, 'index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'},
-                         {'Name': 'другое название', 'Street': 'другая улица', 'District': 'другой район'}],
-        'current_page': current_page,
-        'prev_page_url': None,
-        'next_page_url': next_page_url,
-    })
+    with open(settings.BUS_STATION_CSV, 'r', encoding='cp1251') as file:
+        reader = csv.DictReader(file)
+        bus_station_list = []
+        for row in reader:
+            bus_station_list.append(dict(row))
 
+    paginator = Paginator(bus_station_list, 10)
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
+    articles = paginator.get_page(current_page)
+    data_stations = articles.object_list
+
+    main_url = reverse('bus_stations')
+    previous_page, next_page = None, None
+
+    if articles.has_next():
+        next_page = main_url + f'?page={articles.next_page_number()}'
+    if articles.has_previous():
+        previous_page = main_url + f'?page={articles.previous_page_number()}'
+
+    return render(request, 'index.html', context={
+        'bus_stations': data_stations,
+        'current_page': articles.number,
+        'prev_page_url': previous_page,
+        'next_page_url': next_page,
+    })
